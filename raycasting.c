0962b403da49f8b704405708f7178f010d055698
Zeinab Yaghoubi <z.yaghoubi69@gmail.com>
2026-01-31 15:56:00 +0330

    phase2 : define raycasting loop and DDA algorithm

--- raycasting.c
+++ raycasting.c
@@ -2,7 +2,7 @@
 #include<math.h>
 #define MAP_W 10
 #define MAP_H 10
-#define tilte_size 64
+#define TILE_SIZE 64
 
 int world_map[MAP_H][MAP_W]={
     // defualt map
@@ -34,15 +34,15 @@ int main(){
     const int screenwidth=800;
     const int screenheight=700;
 
-    const float move_speed = 3.0f;
-    const float rot_speed = 3.0f;
-    
     InitWindow(screenwidth,screenheight,"Phase1-step0");
     SetTargetFPS(60);
 
     Player player;
     player.pos = (Vector2){3.5f,3.5f};
     player.dir = (Vector2){1.0f,0.0f};
+
+    const float move_speed = 3.0f;
+    const float rot_speed = 3.0f;
 
     while(!WindowShouldClose()){
 
@@ -120,32 +120,72 @@ int main(){
             player.plane.x = player.plane.x * cos(rotstep) - player.plane.y * sin(rotstep);
             player.plane.y = oldPlaneX * sin(rotstep) + player.plane.y * cos(rotstep);
         }
+        // +++++++++ rendering ++++++++++++
         
         BeginDrawing();
-        ClearBackground(BLACK);
+        ClearBackground(BLUE);
 
-
-        for (int y=0;y<MAP_H;y++){
-
-            for (int x=0;x<MAP_W;x++){
-                int screenX = x * tilte_size;
-                int screenY = y * tilte_size;
-
-                if (world_map[y][x]==1){
-
-                    DrawRectangle(screenX,screenY,tilte_size,tilte_size,GRAY);
-                }
-                else{
-                    DrawRectangle(screenX,screenY,tilte_size,tilte_size,BLACK);
-                }
-
-                DrawRectangleLines(screenX,screenY,tilte_size,tilte_size,DARKGRAY);
+        for (int x = 0; x < screenwidth; x++) {
+            float cameraX = 2.0 * x / screenwidth - 1.0;
+            
+            Vector2 rayDir = {
+                player.dir.x + player.plane.x * cameraX,
+                player.dir.y + player.plane.y * cameraX
+            };
+            
+            // +++ DDA +++
+            int mapX = (int)player.pos.x;
+            int mapY = (int)player.pos.y;
+            
+            double deltaDistX = (rayDir.x ==0) ? 1000000000000000000000000000000.0 : fabs(1/rayDir.x);
+            double deltaDistY = (rayDir.y ==0) ? 1000000000000000000000000000000.0 : fabs(1/rayDir.y);
+            
+            double sideDistX, sideDistY;
+            int stepX, stepY;
+            
+            if (rayDir.x < 0) {
+                stepX = -1;
+                sideDistX = (player.pos.x - mapX) * deltaDistX;
+            } else {
+                stepX = 1;
+                sideDistX = (mapX + 1.0 - player.pos.x) * deltaDistX;
             }
-        }
-        int px = (int) (player.pos.x * tilte_size);
-        int py = (int )(player.pos.y * tilte_size);
+            
+            if (rayDir.y < 0) {
+                stepY = -1;
+                sideDistY = (player.pos.y - mapY) * deltaDistY;
+            } else {
+                stepY = 1;
+                sideDistY = (mapY + 1.0 - player.pos.y) * deltaDistY;
+            }
+            
+            int hit = 0;
+            int side;
+            
+            while (hit == 0) {
+                if (sideDistX < sideDistY) {
+                    sideDistX += deltaDistX;
+                    mapX += stepX;
+                    side = 0;
+                } else {
+                    sideDistY += deltaDistY;
+                    mapY += stepY;
+                    side = 1;
+                }
+                
+                if (mapX >= 0 && mapX < MAP_W && mapY >= 0 && mapY < MAP_H) {
+                    if (world_map[mapY][mapX] > 0) {
+                        hit = 1;
+                    }
+                } else {
+                    hit = 1; // خارج از نقشه
+                }
+            }
 
-        DrawCircle(px,py,tilte_size/4,RED);
+
+        
+
+          
 
         EndDrawing();
     }
